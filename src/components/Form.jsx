b708f51013bd150903931da80db1a3ee83851302
Victor Seleimend <vsennaselei@gmail.com>
2023-02-05 17:28:23 -0300

    Validação

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,59 @@
+import { useState } from 'react';
+import useWeather from '../hooks/useWeather';
+
 const Form = () => {
-  return <div className="container">
-    <form>
-      <div className="field">
-        <label htmlFor="city">Cidade</label>
-        <input type="text" id="city" name="city" />
-      </div>
-      <div className="field">
-        <label htmlFor="country">Cidade</label>
-        <select id="country" name="country">
-          <option value="">-- Selecione um País --</option>
-          <option value="US">Estados Unidos</option>
-          <option value="BR">Brasil</option>
-          <option value="AR">Argentina</option>
-          <option value="CA">Canadá</option>
-          <option value="MX">México</option>
-          <option value="ES">Espanha</option>
-          <option value="DE">Alemanha</option>
-        </select>
-      </div>
-      <input type="submit" value="Consultar Clima" />
-    </form>
-  </div>
-}
+  const [alert, setAlert] = useState('');
+
+  const { search, searchedData, weatherRequest } = useWeather();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (Object.values(search).includes('')) {
+      setAlert('Todos os campos são obrigatórios');
+      return;
+    }
+
+    setAlert('');
+    weatherRequest(search)
+  };
+
+  return (
+    <div className="container">
+      {alert && <p>{alert}</p>}
+      <form onSubmit={handleSubmit}>
+        <div className="field">
+          <label htmlFor="city">Cidade</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            value={search.city}
+            onChange={searchedData}
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="country">Cidade</label>
+          <select
+            id="country"
+            name="country"
+            value={search.country}
+            onChange={searchedData}
+          >
+            <option value="">-- Selecione um País --</option>
+            <option value="US">Estados Unidos</option>
+            <option value="BR">Brasil</option>
+            <option value="AR">Argentina</option>
+            <option value="CA">Canadá</option>
+            <option value="MX">México</option>
+            <option value="ES">Espanha</option>
+            <option value="DE">Alemanha</option>
+          </select>
+        </div>
+        <input type="submit" value="Clima" />
+      </form>
+    </div>
+  );
+};
 
 export default Form;
--- src/context/WeatherProvider.jsx
+++ src/context/WeatherProvider.jsx
@@ -1,10 +1,34 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const WeatherContext = createContext();
 
 const WeatherProvider = ({ children }) => {
+  const [search, setSearch] = useState({
+    city: '',
+    country: ''
+  });
+
+  const searchedData = e => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const weatherRequest = (data) => {
+    console.log(data)
+  }
+
   return (
-    <WeatherContext.Provider value={{}}>{children}</WeatherContext.Provider>
+    <WeatherContext.Provider
+      value={{
+        search,
+        searchedData,
+        weatherRequest
+      }}
+    >
+      {children}
+    </WeatherContext.Provider>
   );
 };
 
