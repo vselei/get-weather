97568aceb9ac24eb1cf9048707f514a06087d086
Victor Seleimend <vsennaselei@gmail.com>
2023-02-05 17:03:11 -0300

    Inicialização

---
+++ src/components/Weather.jsx
@@ -0,0 +1,13 @@
+import Form from "./Form";
+
+const Weather = () => {
+  return (
+    <>
+      <main className="two-columns">
+        <Form />
+      </main>
+    </>
+  );
+};
+
+export default Weather;
