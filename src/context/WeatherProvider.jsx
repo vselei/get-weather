bcb0ec4a4271ef9676f418454dc9797d90210c13
Victor Seleimend <vsennaselei@gmail.com>
2023-02-05 17:57:51 -0300

    consultando api

--- src/context/WeatherProvider.jsx
+++ src/context/WeatherProvider.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from 'react';
+import axios from 'axios';
 
 const WeatherContext = createContext();
 
@@ -7,6 +8,7 @@ const WeatherProvider = ({ children }) => {
     city: '',
     country: ''
   });
+  const [result, setResult] = useState({});
 
   const searchedData = e => {
     setSearch({
@@ -15,16 +17,33 @@ const WeatherProvider = ({ children }) => {
     });
   };
 
-  const weatherRequest = (data) => {
-    console.log(data)
-  }
+  const weatherRequest = async data => {
+    try {
+      const { city, country } = data;
+
+      const appId = import.meta.env.VITE_API_KEY;
+
+      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=${appId}`;
+
+      const { data: response } = await axios(url);
+      const { lat, lon } = response[0];
+
+      const weatherUrl = `https://api.openweathermap.org/data/3.0/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+
+      const { data: weather } = await axios(weatherUrl);
+      setResult(weather);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <WeatherContext.Provider
       value={{
         search,
         searchedData,
-        weatherRequest
+        weatherRequest,
+        result
       }}
     >
       {children}
